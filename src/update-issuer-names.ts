2dbd273ef7fff37bdd2f37e86c4c670426b4bb7e
Jim Toth <jim@artby.city>
2025-03-20 19:39:24 -0400

    Adds immediate renew token flow

--- src/update-issuer-names.ts
+++ src/update-issuer-names.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import https from 'https'
 import fs from 'fs'
 
-export async function updateIssuerNames() {
+export async function setupVaultAxios() {
   const axiosVault = axios.create({
     baseURL: process.env.VAULT_ADDR,
     httpsAgent: new https.Agent({
@@ -12,6 +12,25 @@ export async function updateIssuerNames() {
       'X-Vault-Token': process.env.VAULT_TOKEN
     }
   })
+
+  const response = await axiosVault.post('/v1/auth/token/renew-self', {
+    increment: '2h'
+  })
+
+  console.log('renew-self response.status', response.status)
+  console.log('renew-self response.statusText', response.statusText)
+  console.log('renew-self response.data', response.data)
+
+  if (response.data && response.data.auth && response.data.auth.client_token) {
+    const { auth: { client_token } } = response.data
+    axiosVault.defaults.headers['X-Vault-Token'] = client_token
+  }
+
+  return axiosVault
+}
+
+export async function updateIssuerNames() {
+  const axiosVault = await setupVaultAxios()
 
   const response = await axiosVault.get('/v1/auth/token/lookup-self')
   console.log('response.status', response.status)
